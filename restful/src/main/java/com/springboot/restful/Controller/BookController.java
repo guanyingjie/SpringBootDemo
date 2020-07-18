8b59b4a3229b7d520384da12708b6e0b7b34f646
Yingjie <yingjie.guan@thoughtworks.com>
2020-07-18 14:11:27 +0800

    [yingjie][add RESTful web service

---
+++ restful/src/main/java/com/springboot/restful/Controller/BookController.java
@@ -0,0 +1,32 @@
+package com.springboot.restful.Controller;
+
+import com.springboot.restful.Entity.Book;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class BookController {
+    private List<Book> books = new ArrayList<>();
+
+    @PostMapping("/book")
+    public ResponseEntity<List<Book>> addBook(@RequestBody Book book){
+        books.add(book);
+        return ResponseEntity.ok(books);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteBookById(@PathVariable("id") int id){
+        books.remove(id);
+        return ResponseEntity.ok(books);
+    }
+    @GetMapping("/book")
+    public ResponseEntity getBookByName(@RequestParam("name") String name){
+        List<Book> result = books.stream().filter(book -> book.getName().equals(name)).collect(Collectors.toList());
+        return ResponseEntity.ok(result);
+    }
+
+}
---
+++ restful/src/main/java/com/springboot/restful/Entity/Book.java
@@ -0,0 +1,11 @@
+package com.springboot.restful.Entity;
+
+import lombok.Data;
+
+@Data
+public class Book {
+    private int id;
+    private String name;
+    private String description;
+
+}
